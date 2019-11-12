fb7adc1fb102c8c8b6e5fa3448c17a81b96f553e
amelle <amelle.margaron@gmail.com>
2019-11-12 17:23:37 -0500

    MQS: Added a couple functions

--- MetalQueerSolid/js/script.js
+++ MetalQueerSolid/js/script.js
@@ -7,22 +7,5 @@ var firstlinkDisabled = false;
 var secondLinkDisabled = false;
 
 function choseLink(){
-
-}
-
-if(pageNumber == 1){
-  //display in text box
-  this is pageNumber 1
-}
-
-
-if(pageNumber == 2){
-  //display in text box
-  this is pageNumber 1
-}
-
-
-if(pageNumber == 3){
-  //display in text box
-  this is pageNumber 1
+document.getElementById("changeText").textContent = "CHANGED";
 }
