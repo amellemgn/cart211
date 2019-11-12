396dddfa6e38548b822a676f3786537ddf3473c2
amelle <amelle.margaron@gmail.com>
2019-11-12 16:14:32 -0500

    MQS: Started project.

---
+++ MetalQueerSolid/js/script.js
@@ -0,0 +1,28 @@
+var pageNumber = 0;
+var firstLink = "this is firstLink";
+var secondLink = "this is secondLink";
+
+
+var firstlinkDisabled = false;
+var secondLinkDisabled = false;
+
+function choseLink(){
+
+}
+
+if(pageNumber == 1){
+  //display in text box
+  this is pageNumber 1
+}
+
+
+if(pageNumber == 2){
+  //display in text box
+  this is pageNumber 1
+}
+
+
+if(pageNumber == 3){
+  //display in text box
+  this is pageNumber 1
+}
