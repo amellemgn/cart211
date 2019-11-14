a6ea28651e31a34a02c8a1ef849f4f8c0b6130bf
Amelle <amelle.margaron@gmail.com>
2019-11-14 14:13:07 -0500

    MQS: Small edits

---
+++ MetalQueerSolid/js/Link.js
@@ -0,0 +1,9 @@
+class DialogueOptions {
+constructor(linkName, firstLinkOption, secondLinkOption){
+  this.linkName = linkName;
+  this.firstLinkOption = firstLinkOption;
+  this.secondLinkOption = secondLinkOption;
+}
+
+
+}
--- MetalQueerSolid/js/script.js
+++ MetalQueerSolid/js/script.js
@@ -1,11 +1,17 @@
-var pageNumber = 0;
+// var pageNumber = 0;
+
 var firstLink = "this is firstLink";
 var secondLink = "this is secondLink";
+var thirdLink = "this is thirdLink";
+var fourthLink = "this is fourthLink";
+var fifthLink = "this is fifthLink";
+var sixthLink = "this is sixthLink";
 
-
-var firstlinkDisabled = false;
-var secondLinkDisabled = false;
+//var firstlinkDisabled = false;
+// var secondLinkDisabled = false;
 
 function choseLink(){
 document.getElementById("changeText").textContent = "CHANGED";
+document.getElementById("updateFirstLink").textContent = "NEXTFIRSTLINK";
+document.getElementById("updateSecondLink").textContent = "NEXTSECONDLINK";
 }
