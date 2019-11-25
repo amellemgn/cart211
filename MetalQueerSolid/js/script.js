975c17c0d5dfa680197887230f232ea68bd07b28
amellemgn <55215308+amellemgn@users.noreply.github.com>
2019-11-25 18:12:31 -0500

    MQS: Texting thing almost works

--- MetalQueerSolid/js/script.js
+++ MetalQueerSolid/js/script.js
@@ -1,5 +1,6 @@
 // var pageNumber = 0;
-
+var blueImage = document.getElementById("blueimage");
+var c;
 var firstLinkOption = "this is firstLink bla bla bla lnwernlernlwnrlnerlewnnlwlrnwr";
 var secondLinkOption = "this is secondLink lenenrlntlneltnelrtnlenrltenltnlnelnelnelnelrnt";
 var thirdLinkOption = "this is thirdLink";
@@ -10,6 +11,7 @@ let clicks = 0;
 let dialogOptions = [];
 var speed = 40;
 
+
 dialogOptions.push(new DialogueOptions("test", "Second", "Third", firstLinkOption));
 dialogOptions.push(new DialogueOptions("test2", "Second2", "Third2", secondLinkOption));
 dialogOptions.push(new DialogueOptions("test3", "Second3", "Third3", thirdLinkOption));
@@ -18,18 +20,19 @@ dialogOptions.push(new DialogueOptions("test5", "Second5", "Third5", fifthLinkOp
 dialogOptions.push(new DialogueOptions("test6", "Second6", "Third6", sixthLinkOption));
 
 
-//var firstlinkDisabled = false;
-// var secondLinkDisabled = false;
-
 function choseLink() {
+  removeObjects();
+  addObjects();
   typeWriter(dialogOptions[clicks]);
   //let typed = new Typed('#typed', dialogOptions[clicks].options);
-  document.getElementById("userPara1").textContent = dialogOptions[clicks].username1;
+
+  /*document.getElementById("userPara1").textContent = dialogOptions[clicks].username1;
   document.getElementById("userPara2").textContent = dialogOptions[clicks].username2;
   document.getElementById("changeText").textContent = dialogOptions[clicks].textBoxText;
   document.getElementById("updateFirstLink").textContent = dialogOptions[clicks].firstLinkOption;
-  document.getElementById("updateSecondLink").textContent = dialogOptions[clicks].secondLinkOption;
-  //  clicks++;
+  document.getElementById("updateSecondLink").textContent = dialogOptions[clicks].secondLinkOption;*/
+  clicks++;
+
   console.log("clicks");
 }
 
@@ -43,5 +46,44 @@ function typeWriter(e) {
         typeWriter(e);
       },
       e.speed);
+  }
+}
+/*  if (e.i < e.textBoxText.length && e.textBoxText === dialogOptions[clicks].secondLinkOption) {
+    document.getElementById("blueimage").style.display = "inline";
+  }
+  if (e.i < e.textBoxText.length && e.textBoxText === dialogOptions[clicks].thirdLinkOption || e.textBoxText === dialogOptions[clicks].fifthLinkOption)
+    document.getElementbyId("alert").autoplay == "true";
+}
+*/
+
+function removeObjects() {
+  var myobj = document.getElementById("userPara1");
+  myobj.remove();
+  var myobj1 = document.getElementById("userPara2");
+  myobj1.remove();
+  var myobj2 = document.getElementById("typed");
+  myobj2.remove();
+}
+
+function addObjects() {
+  var para = document.createElement("div");                 // Create a <div> element
+  para.textContent = "This is a paragraph.";
+  para.setAttribute("id", "userPara1");              // Insert text
+  document.getElementById("textingBox").appendChild(para);     // Append <div> to <div> with id="myDIV"
+
+  var para1 = document.createElement("div");
+  para1.textContent = "This is a paragraph.";
+  para1.setAttribute("id", "userPara2");
+  document.getElementById("textingBox").appendChild(para1);
+
+  var para2 = document.createElement("div");
+  para2.textContent = "This is a paragraph.";
+  para2.setAttribute("id", "typed");
+  document.getElementById("textingBox").appendChild(para2);
+}
+
+function mousePressed() {
+  if (document.getElementById("blueimage").style.display = "inline") {
+    document.getElementById("blueimage").style.display = "none";
   }
 }
